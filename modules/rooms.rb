f4d04fc7c89722bfc3e13496825a19f28fdabc5d
Frank Matranga <thefrankmatranga@gmail.com>
2016-06-05 14:56:52 -0400

    Improved join/leave system

--- modules/rooms.rb
+++ modules/rooms.rb
@@ -7,51 +7,49 @@ module RoomCommands
     extend Discordrb::Commands::CommandContainer
 
     # List of special channels
-    joinable = %w(gaming memes meta testing)
-
+    joinable = %w(gaming memes testing)
+	
+	channels_to_roles = {"memes" => "memer", "testing" => "tester", "gaming" => "gamer"}
+	
     command(:join, description: 'Join a special channel. Usage: `!join channelname`') do |event, channel_name|
         server = event.bot.server(150739077757403137)
-
+		user = event.user.on(server)
+		
         if !event.channel.private?
-            channel = server.channels.find { |c| c.name == channel_name }
-
-            if !channel.nil? && joinable.include?(channel_name)
-                Discordrb::API.update_user_overrides(event.bot.token, channel.id, event.user.id, 0, 0)
-                event.message.reply "You have joined #{channel.mention}!"
+            role = server.roles.find{|r| r.name == channels_to_roles[channel_name]}
+            
+            if !role.nil? && joinable.include?(channel_name)
+                user.add_role role
             else
-                event.message.reply "You can only join/leave **#{joinable.join ', '}**. Try `!join memes`"
+                event.user.pm "You can only join/leave **#{joinable.join ', '}**. Try `!join memes`"
             end
         else
             event.user.pm "`!join` and `!leave` are only available in PM's. Try using them here!"
-            event.message.delete
+            
         end
-
+		
+		event.message.delete
         nil
     end
 
     command(:leave, description: 'Leave a special channel. Usage: `!leave channel') do |event, channel_name|
         server = event.bot.server(150739077757403137)
-
+		user = event.user.on(server)
+		
         if !event.channel.private?
-            channel = server.channels.find { |c| c.name == channel_name }
-
-            if !channel.nil? && joinable.include?(channel_name)
-                deny_perms = Discordrb::Permissions.new
-                deny_perms.can_read_messages = true
-                deny_perms.can_send_messages = true
-                deny_perms.can_read_message_history = true
-                deny_perms.can_mention_everyone = true
-
-                Discordrb::API.update_user_overrides(event.bot.token, channel.id, event.user.id, 0, deny_perms.bits)
-                event.message.reply "You have left #{channel.mention}!"
+            role = server.roles.find{|r| r.name == channels_to_roles[channel_name]}
+            
+            if !role.nil? && joinable.include?(channel_name)
+                user.remove_role role
             else
-                event.message.reply "You can only join/leave **#{joinable.join ', '}**. Try `!leave memes`"
+                event.user.pm "You can only join/leave **#{joinable.join ', '}**. Try `!join memes`"
             end
         else
-            event.send_message "`!join` and `!leave` are only available in the server! Try in #meta"
-            event.message.delete
+            event.user.pm "`!join` and `!leave` are only available in PM's. Try using them here!"
+            
         end
-
+		
+		event.message.delete
         nil
     end
 end
