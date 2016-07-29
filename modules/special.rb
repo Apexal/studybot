ff475fd9ef2d4d2daf0efa6fda64d4f179abd11a
Frank Matranga <thefrankmatranga@gmail.com>
2016-07-29 14:03:27 -0400

    Dynamic advisement voice channels

--- modules/special.rb
+++ modules/special.rb
@@ -1,8 +1,51 @@
 module SpecialRoomEvents
   extend Discordrb::EventContainer
 
+  advisements = {}
+  # {
+  #   my id => "2B-1"
+  # }
+
   presence do |event|
     server = event.server
+
+    perms = Discordrb::Permissions.new
+    perms.can_connect = true
+    perms.can_speak = true
+    perms.can_use_voice_activity = true
+
+    # Advisement Voice Channels
+    if event.user.on(server).role? server.roles.find { |r| r.name == 'Verified' }
+      advisement = advisements[event.user.id]
+      if advisement.nil?
+        advisement = $db.query("SELECT advisement FROM students WHERE discord_id=#{event.user.id}").map { |row| row['advisement'][0..1] }.first
+        advisements[event.user.id] = advisement
+      end
+
+      advisement_role = server.roles.find { |r| r.name == advisement }
+      online_count = server.online_members.count { |m| m.role? advisement_role }
+
+      if online_count >= 5
+        puts "Creating voice-channel for Advisement #{advisement}"
+        channel = server.create_channel("Advisement #{advisement}", 'voice')
+        channel.position = 2
+        channel.define_overwrite(advisement_role, perms, 0)
+        Discordrb::API.update_role_overrides($token, channel.id, server.id, 0, perms.bits)
+      elsif online_count <= 2
+        # 1 or 0 online
+        channel = server.voice_channels.find { |v| v.name == advisement }
+        unless channel.nil?
+          puts "Removing voice-channel for Advisement #{advisement}"
+          begin
+            server.text_channels.find { |t| t.id == $hierarchy[channel.id] }.delete
+            $hierarchy.delete channel.id
+          rescue
+            puts 'Failed to find/delete associated #voice-channel'
+          end
+          channel.delete
+        end
+      end
+    end
 
     # Grade Voice Channels
     grades = %w(Freshmen Sophomores Juniors Seniors)
@@ -15,15 +58,11 @@ module SpecialRoomEvents
             puts "Creating voice-channel for #{role.name}"
             channel = server.create_channel(role.name, 'voice')
             channel.position = 2
-            perms = Discordrb::Permissions.new
-            perms.can_connect = true
-            perms.can_speak = true
-            perms.can_use_voice_activity = true
-
             channel.define_overwrite(role, perms, 0)
             Discordrb::API.update_role_overrides($token, channel.id, server.id, 0, perms.bits)
           end
         else
+          channel = server.voice_channels.find { |v| v.name == role.name }
           # 1 or 0 online
           unless channel.nil?
             puts "Removing voice-channel for #{role.name}"
