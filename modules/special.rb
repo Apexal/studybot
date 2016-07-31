176f1864b6a6a364a882e5c54d66df6ba7b6c42e
Frank Matranga <thefrankmatranga@gmail.com>
2016-07-31 14:17:02 -0400

    Dynamic Public Room

--- modules/special.rb
+++ modules/special.rb
@@ -13,7 +13,29 @@ module SpecialRoomEvents
     perms.can_connect = true
     perms.can_speak = true
     perms.can_use_voice_activity = true
-
+    
+    # Public Room
+    guest_role = server.roles.find { |r| r.name == 'Guests' }
+    if event.user.on(server).role? guest_role
+      online_count = server.online_members.count { |m| m.role? guest_role }
+      public_room = server.voice_channels.find { |v| v.name == "Public Room" }
+      if online_count > 0 and public_room.nil?
+        puts "Creating Public Room"
+        public_room = server.create_channel("Public Room", 'voice')
+        public_room.position = 1
+        Discordrb::API.update_role_overrides($token, public_room.id, server.id, perms.bits, 0)
+      elsif online_count == 0 and !public_room.nil?
+        puts "Removing Public Room"
+        begin
+          server.text_channels.find { |t| t.id == $hierarchy[public_room.id] }.delete
+          $hierarchy.delete public_room.id
+        rescue
+          puts 'Failed to find/delete associated #voice-channel'
+        end
+        public_room.delete
+      end
+    end
+    
     # Advisement Voice Channels
     if event.user.on(server).role? server.roles.find { |r| r.name == 'Verified' }
       advisement = advisements[event.user.id]
@@ -24,16 +46,19 @@ module SpecialRoomEvents
 
       advisement_role = server.roles.find { |r| r.name == advisement }
       online_count = server.online_members.count { |m| m.role? advisement_role }
-
-      if online_count >= 5
-        puts "Creating voice-channel for Advisement #{advisement}"
-        channel = server.create_channel("Advisement #{advisement}", 'voice')
-        channel.position = 2
-        channel.define_overwrite(advisement_role, perms, 0)
-        Discordrb::API.update_role_overrides($token, channel.id, server.id, 0, perms.bits)
+      
+      channel = server.voice_channels.find { |v| v.name == advisement }
+      
+      if online_count >= 7
+        if channel.nil?
+          puts "Creating voice-channel for Advisement #{advisement}"
+          channel = server.create_channel("Advisement #{advisement}", 'voice')
+          channel.position = 2
+          channel.define_overwrite(advisement_role, perms, 0)
+          Discordrb::API.update_role_overrides($token, channel.id, server.id, 0, perms.bits)
+        end
       elsif online_count <= 2
         # 1 or 0 online
-        channel = server.voice_channels.find { |v| v.name == advisement }
         unless channel.nil?
           puts "Removing voice-channel for Advisement #{advisement}"
           begin
